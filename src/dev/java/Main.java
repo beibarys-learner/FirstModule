b91851d87f550cf6aa27bd77f191d8c511a078aa
admin <administrator@jits-3-5.jits.ipa.dc>
2022-12-22 18:22:14 +0600

    added more Computer Classs

--- src/dev/java/Main.java
+++ src/dev/java/Main.java
@@ -3,8 +3,11 @@ package dev.java;
 public class Main {
     public static void main(String[] args) {
         Computer computer = new Computer();
+        Computer computer2 = new Computer();
         System.out.println("Computer's name is " + computer.name);
         System.out.println("Computer's system is " + computer.system);
+        System.out.println("Computer's system is " + computer.system);
+        System.out.println("Computer's system is " + computer2.system);
         computer.printVersion();
     }
 }
